230e4375cb7abfd6261a08950b7b7a11c3ff54e8
vertigo2012 <shadman71@gmail.com>
2014-01-25 12:33:29 +0000

    Add GSM

--- omni_d802.mk
+++ omni_d802.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 # Get the long list of APNs
-PRODUCT_COPY_FILES := vendor/omni/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
